60606eeaa9d839bd358ae39581f79f722f2e861f
Meet Patel <pate3857@sheridancollege.ca>
2023-06-29 20:12:49 -0400

    Done GroupOfCards

--- src/game/GroupOfCards.java
+++ src/game/GroupOfCards.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package game;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-/**
- *
- * @author chira
- */
 public class GroupOfCards {
-    
+    private List<Card> cards;
+
+    public GroupOfCards() {
+        cards = new ArrayList<>();
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public void addCards(List<Card> newCards) {
+        cards.addAll(newCards);
+    }
+
+    public int getSize() {
+        return cards.size();
+    }
+
+    public Card drawCard() {
+        if (cards.isEmpty()) {
+            return null;
+        }
+        return cards.remove(cards.size() - 1);
+    }
+
+    public Card getTopCard() {
+        if (cards.isEmpty()) {
+            return null;
+        }
+        return cards.get(cards.size() - 1);
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
 }
