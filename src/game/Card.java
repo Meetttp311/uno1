efa0efdb2bd2fba4eb248aee196dfbe133d605be
Vaibhav Sanghani <sanghanv@sheridancollege.ca>
2023-06-29 20:15:30 -0400

    Content added to Card.java

--- src/game/Card.java
+++ src/game/Card.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package game;
-
-/**
- *
- * @author chira
- */
 public class Card {
-    
+    private String color;
+    private String value;
+
+    public Card(String color, String value) {
+        this.color = color;
+        this.value = value;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return color + " " + value;
+    }
 }
